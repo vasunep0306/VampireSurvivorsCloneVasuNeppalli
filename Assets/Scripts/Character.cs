3a791f3bcd6da2ab9649404bc796a59e04d9f9c5
Vasu Neppalli <vasunep0306@gmail.com>
2023-04-29 09:54:45 -0400

    documentation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -25,6 +25,12 @@ public class Character : MonoBehaviour
         coins.Add(0);
     }
 
+    /// <summary>
+    /// Reduces the character's current health points by the specified amount of damage, after applying armor mitigation.
+    /// If the character's current health points drop to or below zero, the game over screen is displayed and isDead is set to true.
+    /// Updates the character's health bar UI to reflect the new health value.
+    /// </summary>
+    /// <param name="damage">The amount of damage to be applied to the character's health points.</param>
     public void TakeDamage(int damage)
     {
         if(isDead) { return; }
@@ -39,6 +45,11 @@ public class Character : MonoBehaviour
         hpBar.SetState(currentHp, maxHp);
     }
 
+
+    /// <summary>
+    /// Reduces the specified amount of damage by the character's current armor value, and sets the damage value to zero if it becomes negative.
+    /// </summary>
+    /// <param name="damage">The amount of damage to be applied to the character's health points, passed by reference to modify its value.</param>
     private void ApplyArmor(ref int damage)
     {
         damage -= armor;
@@ -48,6 +59,13 @@ public class Character : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// Increases the character's current health points by the specified amount of healing, up to a maximum of maxHp.
+    /// If the character's current health points are zero or below, the method has no effect.
+    /// Updates the character's health bar UI to reflect the new health value.
+    /// </summary>
+    /// <param name="amount">The amount of healing to be applied to the character's health points.</param>
     public void Heal(int amount)
     {
         if(currentHp <= 0) { return; }
