2901bab097eebaa0baac6569d3ab27136ac025f6
Vasanth Neppalli <vasunep0306@gmail.com>
2023-04-30 11:08:39 -0400

    update throwing knives

--- Assets/Scripts/ThrowingKnife.cs
+++ Assets/Scripts/ThrowingKnife.cs
@@ -3,11 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowingKnife : MonoBehaviour
+public class ThrowingKnife : WeaponBase
 {
-    public float timeToAttack;
-    float timer;
-
     public GameObject knifePrefab;
     public PlayerMove playerMove;
 
@@ -16,23 +13,11 @@ public class ThrowingKnife : MonoBehaviour
         if (playerMove == null) { playerMove = GetComponentInParent<PlayerMove>(); }
     }
 
-    private void Update()
-    {
-        if (timer < timeToAttack)
-        {
-            timer += Time.deltaTime;
-            return;
-        }
-        timer = 0;
-        SpawnKnife();
-    }
 
-    private void SpawnKnife()
+    public override void Attack()
     {
         GameObject throwKnife = Instantiate(knifePrefab);
         throwKnife.transform.position = transform.position;
         throwKnife.GetComponent<ThrowingKnifeProjectile>().SetDirection(playerMove.lastHorizontalVector, 0f);
-      
-        
     }
 }
