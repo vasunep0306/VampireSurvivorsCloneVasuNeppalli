9e1b48b181dd5e0ee1ffae550c93da5cb55e7b12
Vasanth Neppalli <vasunep0306@gmail.com>
2023-05-06 13:58:12 -0400

    provide documenation

--- Assets/Scripts/UpgradeData.cs
+++ Assets/Scripts/UpgradeData.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Defines the types of upgrades that can be applied to weapons or items.
+/// </summary>
 public enum UpgradeType
 {
-    WeaponUpgrade,
-    ItemUpgrade,
-    WeaponUnlock,
-    ItemUnlock
+    WeaponUpgrade, // An upgrade that improves the stats of a weapon.
+    ItemUpgrade, // An upgrade that improves the stats of an item.
+    WeaponUnlock, // An upgrade that unlocks a new weapon.
+    ItemUnlock // An upgrade that unlocks a new item.
 }
 
 
+/// <summary>
+/// A scriptable object that stores the data of an upgrade.
+/// </summary>
 [CreateAssetMenu]
 public class UpgradeData : ScriptableObject
 {
-    public UpgradeType upgradeType;
-    public string Name;
-    public Sprite icon;
+    public UpgradeType upgradeType; // The type of the upgrade.
+    public string Name; // The name of the upgrade.
+    public Sprite icon; // The icon of the upgrade.
 
-    public WeaponData weaponData;
-    public WeaponsStats weaponUpgradeStats;
+    public WeaponData weaponData; // The data of the weapon to be upgraded or unlocked.
+    public WeaponsStats weaponUpgradeStats; // The stats of the weapon to be added or improved.
 }
