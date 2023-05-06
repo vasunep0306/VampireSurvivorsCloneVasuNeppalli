49f963acf613f698e05677a15abbe6b8d33ac775
Vasanth Neppalli <vasunep0306@gmail.com>
2023-05-06 14:00:11 -0400

    provide documenation

--- Assets/Scripts/UpgradeButton.cs
+++ Assets/Scripts/UpgradeButton.cs
@@ -4,17 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+/// <summary>
+/// A class that represents a button for selecting an upgrade.
+/// </summary>
 public class UpgradeButton : MonoBehaviour
 {
-    public Image icon;
+    public Image icon; // The image component of the button.
 
+    /// <summary>
+    /// Sets the icon of the button based on the upgrade data.
+    /// </summary>
+    /// <param name="upgradeData">The upgrade data to be displayed.</param>
     public void Set(UpgradeData upgradeData)
     {
         icon.sprite = upgradeData.icon;
     }
 
+    /// <summary>
+    /// Clears the icon of the button.
+    /// </summary>
     public void Clean()
     {
         icon.sprite = null;
     }
 }
+
