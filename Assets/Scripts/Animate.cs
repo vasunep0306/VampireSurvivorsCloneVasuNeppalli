240bfe8a28cde7dfd9b7d998e8bbfd4d1c0b3ae4
Vasanth Neppalli <vasunep0306@gmail.com>
2023-05-06 14:36:59 -0400

    provide documenation

--- Assets/Scripts/Animate.cs
+++ Assets/Scripts/Animate.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A class that controls the animation of a game object.
+/// </summary>
 public class Animate : MonoBehaviour
 {
 
